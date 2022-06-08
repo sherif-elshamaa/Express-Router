d922b259425f86cee1700e2d22c202efa316cf5b
sherif <sherif.elshamaaa@gmail.com>
2022-06-08 13:33:33 +0200

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const workingHours = (req, res, next) => {
     const date = new Date();
     const day = date.getDay();
     const hour = date.getHours();
-    const offdays = [5, 6];
+    const offdays = [0, 6];
     const onHours = [9, 10, 11, 12, 13, 14, 15, 16];
     if (offdays.includes(day)) {
         res.redirect("/on-vacation");
@@ -35,13 +35,13 @@ app.get('/after-hours', (req, res) => {
 app.use(workingHours);
 
 app.get('/', (req, res) => {
-    res.render('home',{img: "public/images/home.jpg"})
+    res.render('home', { img: "public/images/home.jpg" })
 });
 app.get('/our-services', (req, res) => {
-    res.render('service',{img: "public/images/service.jpg"})
+    res.render('service', { img: "public/images/service.jpg" })
 });
 app.get('/contact-us', (req, res) => {
-    res.render('contact',{img: "public/images/contact.jpg"})
+    res.render('contact', { img: "public/images/contact.jpg" })
 });
 
 
